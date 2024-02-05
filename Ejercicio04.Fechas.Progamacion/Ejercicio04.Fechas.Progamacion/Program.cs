91c330535e5424b4bf2dd69cf3775db0c4ae346f
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-02-05 13:45:30 +0100

    Add files via upload

---
+++ Ejercicio04.Fechas.Progamacion/Ejercicio04.Fechas.Progamacion/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Ejercicio04.Fechas.Programacion
+{
+
+
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("FECHA DE NACIMIENTO");
+            DateTime fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+
+
+             int a =   DateTime.Today.CompareTo(fechaNacimiento);
+
+
+
+          
+            if (fechaNacimiento.Day > DateTime.Today.Day)
+            {
+
+
+                if (fechaNacimiento.Month > DateTime.Today.Month)
+                {
+
+                   int anios = fechaNacimiento.Year - DateTime.Today.Year +1;
+     
+
+
+
+                    Console.WriteLine(anios);
+
+                }
+
+
+
+            }
+
+
+            else
+            {
+
+                int anios = fechaNacimiento.Year - DateTime.Today.Year;
+                Console.WriteLine(anios);
+
+            }
+
+
+
+
+
+
+            /**
+            if (a > 0) { 
+            
+            
+            int edad = DateTime.Today.Year - fechaNacimiento.Year -1;
+            Console.WriteLine("EDAD: " + edad);
+
+
+            }
+
+            else if (a == 0) {
+
+
+                int edad = DateTime.Today.Year - fechaNacimiento.Year ;
+
+                Console.WriteLine("EDAD: " + edad);
+
+            }
+
+            else
+            {
+                int edad = DateTime.Today.Year - fechaNacimiento.Year + 1;
+                Console.WriteLine("EDAD: " + edad);
+
+
+
+            }
+            **/
+
+
+
+        }
+
+
+    }
+
+}
